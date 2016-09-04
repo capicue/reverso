d89ddbc76536cb2bcc9ec82932264260ca52adff
Stacey Touset <capicue@gmail.com>
2016-09-03 18:24:41 -0700

    Add hello world elm app

---
+++ elm/Main.elm
@@ -0,0 +1,39 @@
+import Html exposing (..)
+import Html.App as App
+
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- MODEL
+
+type alias Model = {}
+
+init : (Model, Cmd Msg)
+init = ({}, Cmd.none)
+
+-- UPDATE
+
+type Msg = NoOp
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  text "Hello"
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
